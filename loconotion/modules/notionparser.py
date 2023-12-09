e868808f756e6b4ce91f79f6d04f52884808a70a
kmlbgn <kamil.begny@gmail.com>
2023-12-09 11:49:31 +0800

    change chromedriver settings for non-headless (minor debug)

--- loconotion/modules/notionparser.py
+++ loconotion/modules/notionparser.py
@@ -233,7 +233,7 @@ class Parser:
         if not self.args.get("non_headless", False):
             chrome_options.add_argument("--headless")
 
-        chrome_options.add_argument("window-size=1920,1080")
+        chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--disable-dev-shm-usage")
