5edb07ebceeab9f98522cff9048d2badd979ad24
kmlbgn <kamil.begny@gmail.com>
2023-12-09 11:36:15 +0800

    change chromedriver settings for non-headless (remote-debugging)

--- loconotion/modules/notionparser.py
+++ loconotion/modules/notionparser.py
@@ -235,6 +235,7 @@ class Parser:
 
         chrome_options.add_argument("window-size=1920,1080")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--silent")
